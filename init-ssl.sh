177b3727b46dfc25e9a0611fd16f6b455b3663d7
Roman Koval <romankoval10@gmail.com>
2024-06-05 20:35:33 -0400

    give mongodb user permissions to write to mounted volume

--- init-ssl.sh
+++ init-ssl.sh
@@ -2,16 +2,30 @@
 
 SSL_DIR="/data/db/certs"
 
-mkdir -p "$SSL_DIR"
+# Use sudo to create the directory as root
+sudo mkdir -p "$SSL_DIR"
+
+# Use sudo to change ownership as root
+sudo chown mongodb:mongodb "$SSL_DIR"
 
 # Generate Root CA
 openssl req -new -x509 -days "${SSL_CERT_DAYS:-820}" -nodes -text -out "$SSL_DIR/root.crt" -keyout "$SSL_DIR/root.key" -subj "/CN=root-ca" 2>/dev/null
 cat "$SSL_DIR/root.key" "$SSL_DIR/root.crt" > "$SSL_DIR/root.pem"
 
+chown mongodb:mongodb "$SSL_DIR/root.key"
+chown mongodb:mongodb "$SSL_DIR/root.pem"
+chmod 600 "$SSL_DIR/root.key"
+chmod 600 "$SSL_DIR/root.pem"
+
 # Generate Server Certificates
 openssl req -new -nodes -text -out "$SSL_DIR/server.csr" -keyout "$SSL_DIR/server.key" -subj "/CN=localhost" 2>/dev/null
 openssl x509 -req -in "$SSL_DIR/server.csr" -text -out "$SSL_DIR/server.crt" -CA "$SSL_DIR/root.crt" -CAkey "$SSL_DIR/root.key" -CAcreateserial -days "${SSL_CERT_DAYS:-820}" 2>/dev/null
 cat "$SSL_DIR/server.key" "$SSL_DIR/server.crt" > "$SSL_DIR/server.pem"
+
+chown mongodb:mongodb "$SSL_DIR/server.key"
+chown mongodb:mongodb "$SSL_DIR/server.pem"
+chmod 600 "$SSL_DIR/server.key"
+chmod 600 "$SSL_DIR/server.pem"
 
 echo "### ROOT CA CERTIFICATE ###"
 cat "$SSL_DIR/root.pem"
